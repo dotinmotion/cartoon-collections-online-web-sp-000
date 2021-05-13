f0e3550c2c10421ae3ab117f043b58b456c24eed
Dorthy P Thielsen <dotinmotionproductions@gmail.com>
2021-05-13 18:35:54 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,6 +17,6 @@ end
 def find_the_cheese(array)
   cheese_types = ["cheddar", "gouda", "camembert"]
   if array.include?(cheese_types) 
-    array.detect(cheese_types)
+    array.detect{ |food| array.include?
   end
 end
